1532ff381fde36b0c77496db3b96ce5908308da2
Andy Nguyen <andy.nguyen@techneaux.com>
2019-02-14 14:17:47 -0600

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -96,7 +96,7 @@ namespace StarChart.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id){
             var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
-            if (celestialObjects.Any())
+            if (!celestialObjects.Any())
             {
                 return NotFound();
             }
